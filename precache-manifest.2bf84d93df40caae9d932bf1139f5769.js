4786ba75b87c48eb387113ec5406746485a2c83a
Rodrigo Chavez <rodctech@gmail.com>
2019-06-27 11:31:51 -0500

    Updates

---
+++ precache-manifest.2bf84d93df40caae9d932bf1139f5769.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56191edc2516ba92333d004806de21e5",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "9bcf9b7e1fbd6da85386",
+    "url": "/React-Calculator/static/css/main.9f8a7ead.chunk.css"
+  },
+  {
+    "revision": "ca7240506dccb8357ed1",
+    "url": "/React-Calculator/static/js/2.e1819117.chunk.js"
+  },
+  {
+    "revision": "9bcf9b7e1fbd6da85386",
+    "url": "/React-Calculator/static/js/main.d2b0f644.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
